cc70bb6802708b0b54d90091eaa6f6d08e537325
Meaple SFKY <53684805+Meaple-SFKY@users.noreply.github.com>
2021-05-10 22:18:01 +0800

    django-init

--- JS_Folder/WeChat/miniprogram-1/pages/index/index.js
+++ JS_Folder/WeChat/miniprogram-1/pages/index/index.js
@@ -11,6 +11,16 @@ Page({
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
+
+  clickBtn: function() {
+    wx.navigateTo({
+      url: '/pages/demo4/demo4?123',
+      success:res=>{
+        console.log(res)
+      }
+    })
+  },
+
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
