536a33c8a363e57405054fce7a49ff0cd4d217c5
Meaple SFKY <53684805+Meaple-SFKY@users.noreply.github.com>
2021-05-31 03:53:05 +0000

    sc

---
+++ SCup/mySCdjango/spider/views.py
@@ -0,0 +1,33 @@
+import json
+# import os
+import sys
+import base64
+
+# from pathlib import Path
+
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    s = request.POST
+    # prefix = s['img'][:(s['img'].find('base64,') + 6)]
+    baseUrl = s['img'][(s['img'].find('base64,') + 7):]
+
+    imgGer(baseUrl)
+    print(sys.path)
+    print("******************************")
+    data = {
+        "code": '200',
+        "msg": '成功',
+        "data": '微信发送成功',
+        "total": 1
+    }
+    return HttpResponse(json.dumps(data, ensure_ascii=False), content_type="application/json", charset='utf-8', status='200', reason='success')
+
+
+def imgGer(baseUrl):
+    imgdata = base64.b64decode(baseUrl)
+    file = open('1.jpg', 'wb')
+    file.write(imgdata)
+    file.close()
